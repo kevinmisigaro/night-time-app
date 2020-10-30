e536b0fd5510e2b039045f4da169269a9699a4c6
luckykun88 <kmisigaro@outlook.com>
2020-10-30 10:20:24 +0300

    changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:night_life/main.dart';
 
-import 'package:nightversion2/main.dart';
+// import 'package:nightversion2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
